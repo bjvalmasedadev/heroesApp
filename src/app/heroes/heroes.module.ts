91a69541824e59371f1f993dcc3ef68524664da7
Barbaro Javier <barbaro.valmaseda@gmail.com>
2023-06-24 10:49:49 +0400

    feat: create heroes layout

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,9 +7,16 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [HeroPageComponent, LayoutPageComponent, ListPageComponent, NewPageComponent, SearchPageComponent],
-  imports: [CommonModule, HeroesRoutingModule],
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent,
+  ],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,9 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    {
+      label: 'List',
+      icon: 'label',
+      url: './list',
+    },
+    {
+      label: 'Add',
+      icon: 'add',
+      url: './new-hero',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      url: './search',
+    },
+  ];
 }
